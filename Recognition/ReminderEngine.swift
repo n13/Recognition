fb8c19399be129b5996639e9e15ba9c6a9a8f50b
n13 <nheger@gmail.com>
2016-02-18 06:23:16 +0700

    updated pods, changed deployment target to 8.4

--- Recognition/ReminderEngine.swift
+++ Recognition/ReminderEngine.swift
@@ -114,7 +114,6 @@ class ReminderEngine {
         var reminderTimes = [NSDate]()
         var fireTime = startTime
         for _ in 0..<numberOfReminders {
-            fireTime = fireTime.dateByAddingMinutes(minutesPerReminder)
             if fireTime.isLaterThan(nowTime) {
                 reminderTimes.append(fireTime)
                 print("added reminder time: \(fireTime.toLocalString())")
@@ -122,6 +121,7 @@ class ReminderEngine {
                 print("added reminder time for tomorrow: \(fireTime.dateByAddingDays(1).toLocalString())")
                 reminderTimes.append(fireTime.dateByAddingDays(1))
             }
+            fireTime = fireTime.dateByAddingMinutes(minutesPerReminder)
         }
         return reminderTimes
     }
