f4346d51a171ddc3583711d48d58f9f6a634c12d
n13 <nheger@gmail.com>
2016-03-14 16:10:25 +0700

    styled buttons added

---
+++ Recognition/ButtonView.swift
@@ -0,0 +1,14 @@
+//
+//  ButtonView.swift
+//  Recognition
+//
+//  Created by Nikolaus Heger on 3/14/16.
+//  Copyright © 2016 Nikolaus Heger. All rights reserved.
+//
+
+import UIKit
+
+class ButtonView: UIView {
+
+
+}
