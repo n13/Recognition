2b1e37fdc1269f32c00e69b7c7608dd09bb57414
n13 <nheger@gmail.com>
2016-03-15 09:46:35 +0700

    added snapshot, snapshots

--- RecognitionUITests/RecognitionUITests.swift
+++ RecognitionUITests/RecognitionUITests.swift
@@ -8,6 +8,7 @@
 
 import XCTest
 
+@available(iOS 9.0, *)
 class RecognitionUITests: XCTestCase {
         
     override func setUp() {
@@ -17,8 +18,11 @@ class RecognitionUITests: XCTestCase {
         
         // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
+        
         // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
-        XCUIApplication().launch()
+        let app = XCUIApplication()
+        setupSnapshot(app)
+        app.launch()
 
         // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
@@ -28,9 +32,52 @@ class RecognitionUITests: XCTestCase {
         super.tearDown()
     }
     
-    func testExample() {
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testUX() {
+        XCUIDevice.sharedDevice().orientation = .FaceUp
+        XCUIDevice.sharedDevice().orientation = .Portrait
+        
+        
+        let app = XCUIApplication()
+        
+        // HOME SCREEN
+        snapshot("01Home")
+        
+        app.staticTexts["Edit settings."].tap()
+        
+        // SETTINGS SCREEN
+        snapshot("02Settings")
+
+        let scroller = app.scrollViews.elementBoundByIndex(0)
+        scroller.tapAtPosition(CGPoint(x: 100, y: 201))
+        
+        //app.pickerWheels["12, 12 of 50"].tap()
+        
+        // Set number of reminders
+        snapshot("02EditSettings")
+
+        
+        // app.toolbars.buttons["Done"].tap()
+        
+        //app.staticTexts["done"].tap()
+        
+        
     }
     
+    // helper
+    
+
+    func waitFor(element:XCUIElement, seconds waitSeconds:Double) {
+        let exists = NSPredicate(format: "exists == 1")
+        expectationForPredicate(exists, evaluatedWithObject: element, handler: nil)
+        waitForExpectationsWithTimeout(waitSeconds, handler: nil)
+    }
+
+    
 }
+extension XCUIElement /*TapAtPosition*/ {
+    func tapAtPosition(let position: CGPoint) {
+        let cooridnate = self.coordinateWithNormalizedOffset(CGVector(dx: 0, dy: 0)).coordinateWithOffset(CGVector(dx: position.x, dy: position.y))
+        cooridnate.tap()
+    }
+}
+
